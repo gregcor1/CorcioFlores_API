52eae94382e174f590d27dc2939b708dad1c3055
gregcor1 <gregcorcio@gmail.com>
2025-10-26 21:55:12 -0600

    Yo digo que funciona

--- CorcioFlores_20251016/routes/api.php
+++ CorcioFlores_20251016/routes/api.php
@@ -3,16 +3,20 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ZonaController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::post('/nuevousuario', [AuthController::class, 'crearUsuario']);
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::get('/zonas', [ZonaController::class,'obtenerZonas']); //PLURAL
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/usuario', [AuthController::class, 'obtenerUsuario']);
+});
 
-Route::get('/zona/{idzona}', [ZonaController::class,'obtenerZona']); //SINGULAR
-
-Route::get('/zonapais/{idpais}', [ZonaController::class,'obtenerZonaPais']); 
-Route::post('/nuevazona', [ZonaController::class,'obtenerZonaPais']); 
-
+Route::get('/zonas', [ZonaController::class, 'obtenerZonas']);
+Route::get('/zona/{idzona}', [ZonaController::class, 'obtenerZona']);
+Route::get('/zonaspais/{idpais}', [ZonaController::class, 'obtenerZonaPais']);
+Route::post('/nuevazona', [ZonaController::class, 'crearZona']);
